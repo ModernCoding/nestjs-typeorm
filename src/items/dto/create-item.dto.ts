e623f09532c5aa9391a4276fc471538def23191e
ccc.444 <contact@coding-crafting-consulting.com>
2024-07-10 17:30:48 +0700

    create item

--- src/items/dto/create-item.dto.ts
+++ src/items/dto/create-item.dto.ts
@@ -1,1 +1,9 @@
-export class CreateItemDto {}
+// import { CreateListingDto } from './creat-listing.dto';
+// import { CreateTagDto } from './create-tag.dto';
+
+export class CreateItemDto {
+  name: string;
+  public: boolean;
+  // listing: CreateListingDto;
+  // tags: CreateTagDto[];
+}
