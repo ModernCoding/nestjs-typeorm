c4046b1d6f6675266c4962cb65cf25a8f76bd204
ccc.444 <contact@coding-crafting-consulting.com>
2024-07-10 17:21:13 +0700

    no more prettier

--- src/items/entities/item.entity.ts
+++ src/items/entities/item.entity.ts
@@ -1,1 +1,41 @@
-export class Item {}
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  // JoinColumn,
+  // JoinTable,
+  // ManyToMany,
+  // OneToMany,
+  // OneToOne,
+} from "typeorm"
+// import { Listing } from './listing.entity'
+// import { AbstractEntity } from '../../database/abstract.entity'
+// import { Comment } from './comment.entity'
+// import { Tag } from './tag.entity'
+
+@Entity()
+export class Item {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column()
+  name: string
+
+  @Column({ default: true })
+  public: boolean
+
+  constructor(item: Partial<Item>) {
+    Object.assign(this, item)
+  }
+
+  // @OneToOne(() => Listing, { cascade: true })
+  // @JoinColumn()
+  // listing: Listing
+
+  // @OneToMany(() => Comment, (comment) => comment.item, { cascade: true })
+  // comments: Comment[]
+
+  // @ManyToMany(() => Tag, { cascade: true })
+  // @JoinTable()
+  // tags: Tag[]
+}
